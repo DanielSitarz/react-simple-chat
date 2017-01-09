2e775ef0dea62e1f39837dd05ef95c5ffcc07e85
dieow <danielsitarz@gmail.com>
2017-01-09 13:04:18 +0100

    Default object with Object.assign

--- src/helpers/MessagesCreator.js
+++ src/helpers/MessagesCreator.js
@@ -2,14 +2,14 @@ import store from '../store/store'
 
 const messagesCreator = {
   create: (data) => {
-    return {
-      key: data.key || new Date().getTime(),
-      sender: data.sender || 'Anonymous',
-      content: data.content || '',
-      power: data.power || 1.0,
+    return Object.assign({
+      key: new Date().getTime(),
+      sender: 'Anonymous',
+      content: '',
+      power: 1.0,
       roomName: store.getState().chatState.roomName,
-      isFromServer: data.isFromServer || false
-    }
+      isFromServer: false
+    }, data)
   },
   fromServer: function (content) {
     return this.create({
