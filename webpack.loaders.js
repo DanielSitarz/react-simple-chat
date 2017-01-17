93aae983343dc63d5b75a91b735d6291a676d2bf
dieow <danielsitarz@gmail.com>
2017-01-17 21:40:45 +0100

    fixed syntax error while starting app

--- webpack.loaders.js
+++ webpack.loaders.js
@@ -2,7 +2,10 @@ module.exports = [
   {
     test: /\.jsx?$/,
     exclude: /(node_modules|bower_components|public\/)/,
-    loader: 'babel'
+    loader: 'babel',
+    query: {
+      presets:['react', 'es2015']
+    }
   },
   {
     test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
